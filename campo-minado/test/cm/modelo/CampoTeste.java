e913c89813ff02318e41ea968bb98b41cc7e434c
rmls <rodrigomoreiralima@hotmail.com>
2022-08-16 16:02:22 -0300

    classe campo finalizado

--- campo-minado/test/cm/modelo/CampoTeste.java
+++ campo-minado/test/cm/modelo/CampoTeste.java
@@ -1,5 +1,6 @@
 package cm.modelo;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -52,5 +53,72 @@ public class CampoTeste {
 		boolean resultado =  campo.adicionarVizinho(vizinho);
 		assertFalse(resultado);
 	}
-	
+	@Test
+	void testeValorPadraoAtributoMarcado() {
+		assertFalse(campo.isMarcado());
+	}
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	@Test
+	void testeAlternarMarcacaoDuasChamadas() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	@Test
+	void testeAbrirNãoMinadoNãoMarcado() {
+		assertTrue(campo.abrir());
+	}
+	@Test
+	void testeAbrirNãoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrir());
+	}
+	@Test
+	void testeAbrirMinadoMarcado() {
+		campo.alternarMarcacao();
+		campo.minar();
+		assertFalse(campo.abrir());
+	}
+	@Test
+	void testeAbrirMinadoNãoMarcado() {
+		campo.minar();
+		assertThrows(ExplosaoException.class, ()->{
+			campo.abrir();
+		});	
+	}
+	@Test
+	void testeAbrirComVizinhos1() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo22 = new Campo(2,2);
+		
+		campo22.adicionarVizinho(campo11);
+		
+		campo.adicionarVizinho(campo22);
+		
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isAberto());
+		
+	}
+	@Test
+	void testeAbrirComVizinhos2() {
+		Campo campo11 = new Campo(1,1);
+		Campo campo12 = new Campo(1,2);
+		campo12.minar();
+		Campo campo22 = new Campo(2,2);
+		
+		campo22.adicionarVizinho(campo11);
+		campo22.adicionarVizinho(campo12);
+		
+		campo.adicionarVizinho(campo22);
+		
+		campo.abrir();
+		
+		assertTrue(campo22.isAberto() && campo11.isFechado());
+		
+	}
 }
