cc423dc54b43a106f77436c28e627d2b071c8aa7
rmls <rodrigomoreiralima@hotmail.com>
2022-08-19 23:23:03 -0300

    adicionando cm com swing

---
+++ campo-minado-swing/src/cm/modelo/CampoEvento.java
@@ -0,0 +1,6 @@
+package cm.modelo;
+
+public enum CampoEvento {
+
+	ABRIR, MARCAR, DESMARCAR, EXPLODIR, REINICIAR
+}
---
+++ campo-minado-swing/src/cm/modelo/CampoObservador.java
@@ -0,0 +1,7 @@
+package cm.modelo;
+
+@FunctionalInterface
+public interface CampoObservador {
+
+	public void eventoOcorreu(Campo campo, CampoEvento evento);
+}
